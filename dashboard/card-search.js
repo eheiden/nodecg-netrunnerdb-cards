d38f2c3d5f0638e7ff2c1ad1011a34222e806c01
dodgepong <dodgepong@gmail.com>
2018-06-13 01:34:36 -0400

    Add animated card overlay

--- dashboard/card-search.js
+++ dashboard/card-search.js
@@ -7,6 +7,17 @@
     const i_cardQuery = document.getElementById('cardQuery');
     const i_cardPreview = document.getElementById('cardPreview');
 
+    const b_showLeft = document.getElementById('showLeft');
+    const b_hideLeft = document.getElementById('hideLeft');
+    const b_showLeft10s = document.getElementById('showLeft10s');
+    const b_showRight = document.getElementById('showRight');
+    const b_hideRight = document.getElementById('hideRight');
+    const b_showRight10s = document.getElementById('showRight10s');
+    const b_refreshDatabase = document.getElementById('refreshDatabase');
+
+    const r_leftCard = nodecg.Replicant('leftCard');
+    const r_rightCard = nodecg.Replicant('rightCard');
+
     function searchCards(query, cb) {
         const nrdbCards = r_cards.value;
         const cardsData = nrdbCards['data'];
@@ -27,6 +38,36 @@
         }
     }
 
+    function setLeft() {
+        var currentLeftCard = r_leftCard.value;
+        if (i_cardPreview.src === currentLeftCard.img) {
+            return
+        }
+
+        if (i_cardPreview.src == 'no-card.png' || i_cardPreview.src === "") {
+            currentLeftCard.img = "";
+        }
+        else {
+            currentLeftCard.img = i_cardPreview.src;
+        }
+        r_leftCard.value = currentLeftCard;
+    }
+
+    function setRight() {
+        var currentRightCard = r_rightCard.value;
+        if (i_cardPreview.src === currentRightCard.img) {
+            return
+        }
+
+        if (i_cardPreview.src == 'no-card.png' || i_cardPreview.src === "") {
+            currentRightCard.img = "";
+        }
+        else {
+            currentRightCard.img = i_cardPreview.src;
+        }
+        r_rightCard.value = currentRightCard;
+    }
+
     i_cardQuery.addEventListener('keyup', (event) => {
         if (event.keyCode == 13) {
             b_search.click();
@@ -39,14 +80,57 @@
             i_cardPreview.src = "no-card.png";
         } else {
             const nrdbCards = r_cards.value;
-            const imageUrlTemplate = nrdbCards['imageUrlTemplate'];
-
-            if (result['image_url']) {
-                i_cardPreview.src = result['image_url'];
-            } else {
-                i_cardPreview.src = imageUrlTemplate.replace('{code}', result['code']);
-            }
+            i_cardPreview.src = "../shared/netrunnercards/" + result['code'] + ".png";
         }
+    });
+
+    b_refreshDatabase.addEventListener('click', () => {
+        b_refreshDatabase.classList.remove('is-success');
+        b_refreshDatabase.classList.remove('is-danger');
+        b_refreshDatabase.classList.add('is-loading');
+        b_refreshDatabase.disabled = true;
+        axios.get('https://netrunnerdb.com/api/2.0/public/cards')
+            .then(response => {
+                nodecg.Replicant('cards').value = response.data;
+                b_refreshDatabase.classList.remove('is-loading');
+                b_refreshDatabase.classList.add('is-success');
+                b_refreshDatabase.disabled = false;
+            })
+            .catch(error => {
+                b_refreshDatabase.classList.remove('is-loading');
+                b_refreshDatabase.classList.add('is-danger');
+                b_refreshDatabase.disabled = false;
+
+            });
+    });
+
+    b_showLeft.addEventListener('click', () => {
+        console.log('clik');
+        setLeft();
+        nodecg.sendMessage('showLeft');
+    });
+
+    b_hideLeft.addEventListener('click', () => {
+        nodecg.sendMessage('hideLeft');
+    });
+
+    b_showLeft10s.addEventListener('click', () => {
+        setLeft();
+        nodecg.sendMessage('showLeft10s');
+    });
+
+    b_showRight.addEventListener('click', () => {
+        setRight();
+        nodecg.sendMessage('showRight');
+    });
+
+    b_hideRight.addEventListener('click', () => {
+        nodecg.sendMessage('hideRight');
+    });
+
+    b_showRight10s.addEventListener('click', () => {
+        setRight();
+        nodecg.sendMessage('showRight10s');
     });
 
     autocomplete('#cardQuery', { hint: false, autoselect: true, appendTo: '#cardQueryAutocomplete' }, [
--- extension.js
+++ extension.js
@@ -3,6 +3,19 @@
 module.exports = function (nodecg) {
     const axios = require('axios');
 
+    const leftInitialValue = {
+        img: ""
+
+    };
+    const rightInitialValue = {
+        img: ""
+    };
+
+    const r_leftCard = nodecg.Replicant('leftCard', { defaultValue: leftInitialValue });
+    const r_rightCard = nodecg.Replicant('rightCard', { defaultValue: rightInitialValue });
+
     axios.get('https://netrunnerdb.com/api/2.0/public/cards')
         .then(response => nodecg.Replicant('cards').value = response.data);
+
+
 };
