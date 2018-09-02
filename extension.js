9b7f194a8c34b5f3bbcbad7cc0cd026694226058
dodgepong <dodgepong@gmail.com>
2018-09-02 03:31:24 -0400

    Load cards from local machine instead of NRDB
    In case it goes down, as has been doing lately.
    Also lets me customize the content to add world champ cards.

--- extension.js
+++ extension.js
@@ -2,6 +2,10 @@
 
 module.exports = function (nodecg) {
     const axios = require('axios');
+    const fs = require('fs');
+    const path = require('path');
+
+    const filePath = path.join(__dirname, 'shared/cards.json');
 
     const leftInitialValue = {
         img: ""
@@ -18,8 +22,11 @@ module.exports = function (nodecg) {
     const r_rightCard = nodecg.Replicant('rightCard', { defaultValue: rightInitialValue });
     const r_soloCard = nodecg.Replicant('soloCard', { defaultValue: soloInitialValue });
 
-    axios.get('https://netrunnerdb.com/api/2.0/public/cards')
-        .then(response => nodecg.Replicant('cards').value = response.data);
-
-
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (!err) {
+            const cards = nodecg.Replicant('cards').value = JSON.parse(data);
+        } else {
+            console.log(err);
+        }
+    });
 };
